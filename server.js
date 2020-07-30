f1876b29e1d4d792ef552a3612c65e057996cd17
Giovanni Noisy <gnoisy77@gmail.com>
2020-07-29 20:31:24 -0700

    updated server comments

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 // Requiring necessary npm packages
+
+//express is required -- allows javascript to be run outside of browser
 var express = require("express");
+
+//express-session is used to store user data between http requests
 var session = require("express-session");
-// Requiring passport as we've configured it
+
+// passport is middleware for node that is used for authentication
 var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
@@ -10,9 +15,16 @@ var db = require("./models");
 
 // Creating express app and configuring middleware needed for authentication
 var app = express();
+
+//allows the use of nested objects if extended is true, cannot if false
 app.use(express.urlencoded({ extended: true }));
+
+//recognizes incoming request as json object
 app.use(express.json());
+
+//allows the use of what's inside public folder
 app.use(express.static("public"));
+
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
